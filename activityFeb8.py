fff7da86ca535bd9cb5c3e3adb0544465c649dc1
ZWallace012 <158314474+ZWallace012@users.noreply.github.com>
2024-02-08 17:43:30 +0000

    Daily Activity

---
+++ activityFeb8.py
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+number_A = int('15')
+number_B = int('30')
+print(number_A + number_B)
+
+
+isMember = True
+goldTier = True
+
+print(isMember and goldTier)
+
+fruit_A = apples
+fruit_B = oranges
