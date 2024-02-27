9ee31c199328f30b95388ecc54ffc1af904f81cc
ZWallace012 <158314474+ZWallace012@users.noreply.github.com>
2024-02-27 16:41:31 +0000

    daily warm up

---
+++ warmup_Feb23.py
@@ -0,0 +1,8 @@
+# Function Definition --> defines the functions. Tells the program how you functions work.
+Saves the instuctions and runs it when called
+
+# Function Call --> Execute functions. The program will run the function
+
+# A function parameter is the variable listed in the parentheses.
+# A function arguement is the value that is sent to the function when its calls
+
---
+++ warmup_feb20.py
@@ -0,0 +1,10 @@
+a = Detergent
+b = DryerSheets
+c = Softener
+d = WashingMachine
+e = Dryer
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
